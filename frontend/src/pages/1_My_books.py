aa49057378de0429580ad2bff5b5f42a6936cd9f
Artemiy Kochergin <kocherginartem@mail.ru>
2024-04-20 11:05:27 +0300

    add search by my books

--- frontend/src/pages/1_My_books.py
+++ frontend/src/pages/1_My_books.py
@@ -2,58 +2,98 @@ import streamlit as st
 
 from model.book import Book
 
+books_list_key = "books_list"
+search_string_key = "search_string"
+
 
 @st.cache_data
 def get_books():
+    # TODO: add a query to backend here
     books = [
-        Book("Book name", "Author", 40),
-        Book("Book name1", "Author1", 0),
-        Book("Book name2", "Author2", 100),
-        Book("Book name3", "Author3", 15)
+        Book("A Tale of Two Cities", "Charles Dickens", "Historical fiction", 40, True),
+        Book("The Little Prince", "Antoine de Saint-Exupéry", "Fantasy, Children's fiction", 0, True),
+        Book("The Alchemist", "Paulo Coelho", "Fantasy", 100, True),
+        Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", "Fantasy, Children's fiction", 15, True),
+        Book("A Tale of Two Cities", "Charles Dickens", "Historical fiction", 40, True),
+        Book("The Little Prince", "Antoine de Saint-Exupéry", "Fantasy, Children's fiction", 0, True),
+        Book("The Alchemist", "Paulo Coelho", "Fantasy", 100, True),
+        Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", "Fantasy, Children's fiction", 15, True),
+        Book("A Tale of Two Cities", "Charles Dickens", "Historical fiction", 40, True),
+        Book("The Little Prince", "Antoine de Saint-Exupéry", "Fantasy, Children's fiction", 0, True),
+        Book("The Alchemist", "Paulo Coelho", "Fantasy", 100, True),
+        Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", "Fantasy, Children's fiction", 15, True),
     ]
     return books
 
 
+@st.cache_data
+def get_filtered_books(search_string: str):
+    books = get_books()
+
+    if not search_string:
+        return books
+
+    # TODO: add a query to backend here
+    return [book for book in books if book.name.startswith(search_string)]
+
+
 def top_bar():
+    if search_string_key not in st.session_state:
+        st.session_state[search_string_key] = ""
+
     with st.container(border=False):
         cols = st.columns([1, 4, 1])
 
         with cols[0]:
             find_books_button = st.button("Find books")
             if find_books_button:
-                search_string = st.session_state["search_string"]
-                books = get_books()
-                st.session_state["filtered_books"] = [book for book in books if book.name.startswith(search_string)]
+                st.session_state.pop(books_list_key)
+                st.switch_page("pages/2_Find_books.py")
 
         with cols[1]:
-            st.text_input(label="Search string", placeholder="Type a book's title...", key="search_string", label_visibility="collapsed")
+            current_search_string = st.text_input(
+                label="Search string",
+                placeholder="Type a book's title...",
+                key="current_search_string",
+                label_visibility="collapsed"
+            )
+
+            if current_search_string is None:
+                current_search_string = ""
+
+            if current_search_string != st.session_state[search_string_key]:
+                st.session_state[search_string_key] = current_search_string
+                st.session_state[books_list_key] = get_filtered_books(current_search_string)
 
         with cols[2]:
             logout_button = st.button("Logout")
             if logout_button:
-               st.switch_page("Home.py")
+                st.switch_page("Home.py")
 
 
 def my_books_page():
-    books = get_books()
-
-    if "filtered_books" in st.session_state:
-        books = st.session_state["filtered_books"]
+    if books_list_key not in st.session_state:
+        st.session_state[books_list_key] = get_books()
 
     st.title("My books")
 
     top_bar()
 
+    books = st.session_state[books_list_key]
+
     for book in books:
         with st.container(border=True):
-            cols = st.columns(2)
+            cols = st.columns(3)
 
             with cols[0]:
                 st.write(book.name)
                 st.write(book.author)
 
             with cols[1]:
-                st.checkbox(label="In my books", value=True, key=book.name + str(books.index(book)))
+                st.write(book.genre)
+
+            with cols[2]:
+                st.checkbox(label="In my books", value=book.in_my_collection, key=book.name + str(books.index(book)))
 
             st.progress(book.progress)
 
