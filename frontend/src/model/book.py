500814ec677c1d7015e20c542db3fc57d148ff30
Artemiy Kochergin <kocherginartem@mail.ru>
2024-04-20 11:05:54 +0300

    add fields to book

--- frontend/src/model/book.py
+++ frontend/src/model/book.py
@@ -1,6 +1,8 @@
 class Book:
-    def __init__(self, name, author, progress):
+    def __init__(self, name, author, genre, progress, in_my_collection):
         self.name = name
         self.author = author
+        self.genre = genre
         self.progress = progress
+        self.in_my_collection = in_my_collection
 
