9d7b848bf3bc08f600c5770afdb195a6e20f7ef0
Artemiy Kochergin <kocherginartem@mail.ru>
2024-04-24 19:20:32 +0300

    add setting progress, fix cookie error

--- frontend/src/model/book.py
+++ frontend/src/model/book.py
@@ -1,25 +1,23 @@
 class Book:
-    def __init__(self, book_id: int, name: str, author: str, genre: str, progress: int, in_my_collection: bool):
+    def __init__(self, book_id: int, name: str, author: str, genre: str, pages_read: int, pages_total: int, in_my_collection: bool):
         self.id = book_id
         self.name = name
         self.author = author
         self.genre = genre
-        self.progress = progress
+        self.pages_read = pages_read
+        self.pages_total = pages_total
         self.in_my_collection = in_my_collection
 
 
 def parse_book(json_book) -> Book:
-    pages_total = int(json_book["total_pages"])
-    pages_read = int(json_book["read_pages"])
-
-    progress = int(pages_read / pages_total * 100)
 
     return Book(
         json_book["id"],
         json_book["title"],
         json_book["author"],
         json_book["genre"],
-        progress,
+        int(json_book["read_pages"]),
+        int(json_book["total_pages"]),
         json_book["in_collection"] if "in_collection" in json_book else True
     )
 
