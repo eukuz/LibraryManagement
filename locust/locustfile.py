49c9720eed6804666e7ee921a7507460fb15c60a
Ilya Siluyanov <isiluyanov8@gmail.com>
2024-04-30 12:31:35 +0300

    Add more tests (#7)
    * Try to win some mutations
    
    * locust tests
    
    * radon 10
    
    ---------
    
    Co-authored-by: Inga Ezhova <i.ezhova@innopolis.university>

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -10,6 +10,23 @@ class QuickstartUser(HttpUser):
         for offset in range(0, 13000, 50):
             self.client.get("/api/books/", params={'limit': 20, 'offset': offset})
 
+    @task
+    def get_collection(self):
+        self.client.get("/api/collection")
+
+    @task
+    def get_book_details(self):
+        book_id = 123  # Replace with a valid book ID
+        self.client.get(f"/api/books/{book_id}")
+
+    @task
+    def suggest_books(self):
+        self.client.get("/api/books/suggest")
+
+    @task
+    def get_root_books(self):
+        self.client.get("/api/books/")
+
     def on_start(self):
         session_id = os.environ['SESSION_ID']
         self.client.headers = {'Authorization': f'Bearer {session_id}'}
